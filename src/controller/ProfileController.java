7b903b78711515a13a131419a3008618045a2d8e
Maani Aldaghi <maanialdaghi@gmail.com>
2026-06-14 09:03:31 +0330

    feature(ProfileController.java):added methods

---
+++ src/controller/ProfileController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import model.user.User;
+
+public class ProfileController {
+
+
+    public void changeUsername(User user) {}
+
+    public void changePassword(User user,String oldPassword, String newPassword) {}
+
+    public void changeNickname(User user, String nickname) {}
+
+    public void changeEmail(User user, String email) {}
+
+    public void Show_info(User user) {}
+}
