f275d6a29e0fa2e83fdbbf2a7239428823966328
Maani Aldaghi <maanialdaghi@gmail.com>
2026-07-14 04:07:35 +0330

    fix: returned ProjectileType.java

---
+++ src/model/data/projectile/ProjectileType.java
@@ -0,0 +1,22 @@
+package model.data.projectile;
+
+public enum ProjectileType {
+    PEA,
+    ICE,
+    FIRE,
+    SPIKE,
+    FUME,
+    LASER,
+    STAR,
+    CABBAGE,
+    MELON,
+    FREEZE_LINE,
+    ROTO_SEED,
+    OCTOPUS,
+    PLASMA,
+    BOUNCING_AQUA,
+    BOUNCING_ORANGE,
+    BOUNCING_BLUE,
+    SUPER_BULB,
+    POISON
+}
