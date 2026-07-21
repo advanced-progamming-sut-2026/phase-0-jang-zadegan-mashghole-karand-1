82721db78f1dae1ed1c4e4a37cb1f28b9f2f8b4b
Maani Aldaghi <maanialdaghi@gmail.com>
2026-07-22 01:06:05 +0330

    feat: added zombie stun and octopus ability

--- src/model/data/zombie/abilities/runtime/ZombieStunAbility.java
+++ src/model/data/zombie/abilities/runtime/ZombieStunAbility.java
@@ -2,14 +2,34 @@ package model.data.zombie.abilities.runtime;
 
 import model.core.EventBus;
 import model.core.GameState;
+import model.data.plant.Plant;
+import model.data.plant.stuns.CatStun;
+import model.data.plant.stuns.StunKind;
 import model.data.zombie.Zombie;
 import model.data.zombie.abilities.config.ZombieAbilityConfig;
 
+import java.util.List;
+import java.util.Random;
+
 public class ZombieStunAbility implements ZombieAbilityConfig {
+    Random rand = new Random();
+    private final int COOLDOWN = 50;
+    private int cooldown = COOLDOWN;
 
     @Override
     public void onTick(Zombie zombie, GameState state, EventBus bus) {
-
+        if(cooldown> 0){
+            cooldown--;
+            return;
+        }
+        List<Plant> plants = state.getPlants().stream()
+                .filter(p -> p.getActiveStun() ==null || p.getActiveStun().getKind() != StunKind.CAT)
+                .toList();
+        if(!plants.isEmpty()){
+            Plant target = plants.get(rand.nextInt(plants.size()));
+            target.applyStun(new CatStun(zombie));
+            cooldown = COOLDOWN;
+        }
     }
 
     @Override
--- src/model/data/zombie/abilities/runtime/ZombieThrowAbility.java
+++ src/model/data/zombie/abilities/runtime/ZombieThrowAbility.java
@@ -4,12 +4,18 @@ import model.core.EventBus;
 import model.core.GameLoop;
 import model.core.GameState;
 import model.core.Position;
+import model.data.plant.Plant;
 import model.data.plant.abilities.config.Direction;
+import model.data.plant.stuns.StunKind;
+import model.data.projectile.LobbedProjectile;
 import model.data.projectile.Projectile;
 import model.data.projectile.ProjectileTarget;
 import model.data.projectile.ProjectileType;
 import model.data.zombie.Zombie;
+import model.data.zombie.ZombieType;
 import model.data.zombie.abilities.config.ZombieAbilityConfig;
+
+import java.util.Comparator;
 
 public class ZombieThrowAbility implements ZombieAbilityConfig {
 
@@ -33,14 +39,33 @@ public class ZombieThrowAbility implements ZombieAbilityConfig {
             return;
         }
 
-        boolean hasTarget = state.plants.stream().anyMatch(plant -> plant.row == zombie.row);
-        if (hasTarget) {
-            Projectile projectile = new Projectile(damage,
-                    new Position(zombie.position.x, zombie.position.y),
-                    zombie.row, zombie.col, speed, projectileType,
-                    ProjectileTarget.PLANT);
-            projectile.setDirection(Direction.BACK);
-            state.projectiles.add(projectile);
+        if(zombie.type == ZombieType.HUNTER) {
+            boolean hasTarget = state.plants.stream().anyMatch(plant -> plant.row == zombie.row);;
+            if (hasTarget) {
+                Projectile projectile = new Projectile(damage,
+                        new Position(zombie.position.x, zombie.position.y),
+                        zombie.row, zombie.col, speed, projectileType,
+                        ProjectileTarget.PLANT);
+                projectile.setDirection(Direction.BACK);
+                state.projectiles.add(projectile);
+            }
+        } else if (zombie.type == ZombieType.OCTOPUS_ZOMBIE) {
+            Plant target = state.plants.stream()
+                    .filter(plant -> plant.row == zombie.row && (plant.getActiveStun() ==null
+                            || plant.getActiveStun().getKind()!= StunKind.OCTOPUS))
+                    .max(Comparator.comparingDouble(p ->p.getX())).orElse(null);
+            if (target != null) {
+                Projectile p = new LobbedProjectile(0,new Position(zombie.position.x, zombie.position.y),
+                        target.row,target.col,
+                        speed,
+                        ProjectileType.OCTOPUS,
+                        ProjectileTarget.PLANT,
+                        new Position(target.getX(), target.getY()),
+                        0f,50f,60f,0f,0,0);
+                        p.direction = Direction.BACK;
+                        state.projectiles.add(p);
+            }
+
         }
         currentCooldown = (int) (cooldownSeconds * GameLoop.TICKS_PER_SECOND);
     }
